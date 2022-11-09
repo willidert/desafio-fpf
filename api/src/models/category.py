001432f54c5084f37db857b19aa88f82cc3eeae2
willidert <williderteixeira@gmail.com>
2022-11-08 21:33:30 -0400

    style: update models

--- api/src/models/category.py
+++ api/src/models/category.py
@@ -5,6 +5,7 @@ from src.db.base_class import Base
 
 class Category(Base):
     __tablename__ = "categories"
+    
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(length=225), nullable=False)
 
