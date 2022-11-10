f8d2f589e71e0e617f8f9ef83e5dbe5fccf1f3e7
willidert <williderteixeira@gmail.com>
2022-11-09 21:39:41 -0400

    feat: create all crud operations

--- api/src/services/categoriesService.py
+++ api/src/services/categoriesService.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from sqlalchemy.orm import Session
 
 from src.models.category import Category
@@ -7,3 +7,21 @@ from src.schemas import categorySchema
 
 def get_categories(db: Session, skip=0, limit=100) -> List[categorySchema.Category]:
     return db.query(Category).offset(skip).limit(limit).all()
+
+def get_category_by_id(db: Session, id: int) -> Optional[categorySchema.Category]:
+    return db.query(Category).where(Category.id == id)
+
+def create_category(db: Session, category: categorySchema.CategoryCreate) -> categorySchema.Category:
+    db_category = Category(category.dict())
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+def update_category(db: Session, category: categorySchema.CategoryUpdate, id: int) -> None:
+    db.query(Category).filter(Category.id == id).update(category.dict(exclude_unset=True))
+    db.commit()
+
+def delete_category(db: Session, id: int) -> None:
+    db.query(Category).filter(Category.id == id).delete()
+    db.commit()
--- api/src/services/productService.py
+++ api/src/services/productService.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from sqlalchemy.orm import Session
 
 from src.models.product import Product
@@ -7,3 +7,21 @@ from src.schemas import productSchema
 
 def get_products(db: Session, skip=0, limit=100) -> List[productSchema.Product]:
     return db.query(Product).offset(skip).limit(limit).all()
+
+def get_product_by_id(db: Session, id: int) -> Optional[productSchema.Product]:
+    return db.query(Product).where(Product.id == id)
+
+def create_product(db: Session, product: productSchema.ProductCreate) -> productSchema.Product:
+    db_product = Product(product.dict)
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def update_product(db: Session, product: productSchema.ProductUpdate, id: int) -> None:
+    db.query(Product).filter(Product.id == id).update(product.dict(exclude_unset=True))
+    db.commit()
+
+def delete_product(db: Session, id: int) -> None:
+    db.query(Product).filter(Product.id == id).delete()
+    db.commit()
