ce88d0c37d0d89d458a77985649089ce2371211a
William Teixeira <williderteixeira@gmail.com>
2022-11-10 20:16:00 -0400

    feat: create product service and add routes

---
+++ web/src/app/products/model/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id?: number;
+  price: number;
+  purchase_date: Date;
+  description: string;
+}
---
+++ web/src/app/products/services/product.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private readonly API_URL = 'localhost:8000/api/products/';
+  constructor(private client: HttpClient) {}
+
+  get_products(): Observable<Product[]> {
+    return this.client.get<Product[]>(this.API_URL);
+  }
+
+  get_product_by_id(id: number): Observable<Product> {
+    return this.client.get<Product>(`${this.API_URL}${id}`);
+  }
+
+  save(product: Partial<Product>) {
+    if (product.id) {
+      return this.update(product);
+    }
+
+    return this.create(product);
+  }
+
+  private create(product: Partial<Product>) {
+    return this.client.post(this.API_URL, product);
+  }
+
+  private update(product: Partial<Product>) {
+    return this.client.put<Product>(`${this.API_URL}${product.id}`, product);
+  }
+}
