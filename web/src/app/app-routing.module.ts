89db1b7da3517aab079e6ba6aac852479a2d38f8
William Teixeira <williderteixeira@gmail.com>
2022-11-13 00:06:03 -0400

    refactor: remove unnecessary component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -8,11 +8,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
   },
-  {
-    path: 'categories',
-    loadChildren: () =>
-      import('./categories/categories.module').then((m) => m.CategoriesModule),
-  },
 ];
 
 @NgModule({
