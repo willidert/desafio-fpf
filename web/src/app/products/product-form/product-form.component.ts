0b077f60f054b769026acabffb29fa9219b567c7
William Teixeira <williderteixeira@gmail.com>
2022-11-13 00:14:17 -0400

    feat: create product form

--- web/src/app/products/product-form/product-form.component.ts
+++ web/src/app/products/product-form/product-form.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss']
+  styleUrls: ['./product-form.component.scss'],
 })
 export class ProductFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  productForm: FormGroup;
+  constructor() {
+    this.productForm = new FormGroup({
+      id: new FormControl('', { nonNullable: true }),
+      category: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.maxLength(225)],
+      }),
+      description: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.maxLength(225)],
+      }),
+      price: new FormControl(0, {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      purchaseDate: new FormControl<Date | null>(null, {
+        validators: [Validators.required],
+        nonNullable: true,
+      }),
+    });
   }
 
+  ngOnInit(): void {}
 }
--- web/src/app/products/products.module.ts
+++ web/src/app/products/products.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
-import { ProductFormComponent } from './product-form/product-form.component';
-
 
 @NgModule({
-  declarations: [
-    ProductsComponent,
-    ProductFormComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+  declarations: [ProductsComponent, ProductFormComponent],
+  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
