e116bd2581c73c86e7e15b4c04600cd77ba871a5
William Teixeira <williderteixeira@gmail.com>
2022-11-13 00:13:42 -0400

    feat: list products

--- web/src/app/products/products/products.component.ts
+++ web/src/app/products/products/products.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Product } from '../model/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  products$: Observable<Product[]>;
+  constructor(
+    private service: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.products$ = this.service.get_products();
   }
 
+  ngOnInit(): void {
+    this.products$ = this.service.get_products();
+  }
+
+  onEdit(id: string): void {
+    console.log(`product ${id} editado`);
+  }
+
+  onDelete(id: string): void {
+    console.log(`product ${id} deletado`);
+  }
+
+  onAdd(): void {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
 }
