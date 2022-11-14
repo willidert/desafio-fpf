ad5599f2deb766b637b93dbb79dc53b7b3da1392
William Teixeira <williderteixeira@gmail.com>
2022-11-13 22:30:33 -0400

    chore: add angular material

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
     CurrencyMaskModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
